6972fc8b6d506521b33257a7fa67b01169445b0d
harshadvali11 <66057460+harshadvali11@users.noreply.github.com>
2020-10-27 21:34:55 -0700

    first commit

---
+++ app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+from app.forms import *
+
+def topicform(request):
+    form=TopicForm()
+    if request.method=='POST':
+        form_data=TopicForm(request.POST)
+        if form_data.is_valid():
+            form_data.save()
+
+    return render(request,'topicform.html',context={'form':form})
+
+
+def webpageform(request):
+    form=WebpageForm()
+    if request.method=="POST":
+        form_data=WebpageForm(request.POST)
+        if form_data.is_valid():
+            form_data.save()
+    return render(request,'webpageform.html',context={'form':form})
